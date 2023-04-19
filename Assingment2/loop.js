76a12300f986e286de061357668a2e1ab0b905c3
mahiksy <97805518+mahiksy@users.noreply.github.com>
2023-04-19 19:02:52 -0400

    second

---
+++ Assingment2/loop.js
@@ -0,0 +1,18 @@
+function biggerNumber(x,y){
+    if(x>y) console.log(x);
+    else console.log(y);
+}
+
+biggerNumber(4,5)
+biggerNumber(110,50)
+
+function evenOdd(x){
+    if(x%2==0){
+        console.log(`${x} is Even!`)
+    }
+    else{
+        console.log(`${x} is Odd!`)
+    }
+}
+evenOdd(5); 
+evenOdd(16); 
